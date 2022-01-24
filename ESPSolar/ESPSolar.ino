cefc20b2545eaaef9c7634c81041107806ce59a0
denisko28 <39884531+denisko28@users.noreply.github.com>
2022-01-24 16:30:27 +0200

    Update ESPSolar.ino

--- ESPSolar/ESPSolar.ino
+++ ESPSolar/ESPSolar.ino
@@ -68,38 +68,33 @@ String sendRequest(String resourcePath, bool needPayload)
   return response;
 }
 
-void light_sleep() {
-  gpio_pin_wakeup_enable(GPIO_ID_PIN(2), GPIO_PIN_INTR_LOLEVEL);
-  wifi_set_opmode(NULL_MODE);
-  wifi_fpm_set_sleep_type(LIGHT_SLEEP_T);
-  wifi_fpm_open();
-  wifi_fpm_do_sleep(0xFFFFFFF);
-  delay(1000);
-
-  Serial.print("Wakeup^^");
-  initWifi();
-  wifi_set_sleep_type(NONE_SLEEP_T);
-}
-
-//void light_sleep()
-//{
-//   //wifi_station_disconnect();
-//   Serial.print("a^^");
-//   gpio_pin_wakeup_enable(GPIO_ID_PIN(2), GPIO_PIN_INTR_LOLEVEL); // GPIO_ID_PIN(2) corresponds to GPIO2 on ESP8266-01 , GPIO_PIN_INTR_LOLEVEL for a logic low, can also do other interrupts, see gpio.h above
-//   Serial.print("b^^");
-//   wifi_set_opmode_current(NULL_MODE);
-//   Serial.print("c^^");
-//   wifi_fpm_set_sleep_type(LIGHT_SLEEP_T); // set sleep type, the above    posters wifi_set_sleep_type() didnt seem to work for me although it did let me compile and upload with no errors 
-//   Serial.print("d^^");
-//   wifi_fpm_open(); // Enables force sleep
-//   Serial.print("e^^");
-//   wifi_fpm_do_sleep(0xFFFFFFF); // Sleep for longest possible time
-//   delay(1000);
-//   
-//   // on Wakeup
-//   Serial.print("Wakeup^^");
-//   initWifi();
+//void light_sleep() {
+//  gpio_pin_wakeup_enable(GPIO_ID_PIN(2), GPIO_PIN_INTR_LOLEVEL);
+//  wifi_set_opmode(NULL_MODE);
+//  wifi_fpm_set_sleep_type(LIGHT_SLEEP_T);
+//  wifi_fpm_open();
+//  wifi_fpm_do_sleep(0xFFFFFFF);
+//  delay(1000);
+//
+//  Serial.print("Wakeup^^");
+//  initWifi();
+//  wifi_set_sleep_type(NONE_SLEEP_T);
 //}
+
+void light_sleep()
+{
+   wifi_station_disconnect();
+   gpio_pin_wakeup_enable(GPIO_ID_PIN(2), GPIO_PIN_INTR_LOLEVEL); // GPIO_ID_PIN(2) corresponds to GPIO2 on ESP8266-01 , GPIO_PIN_INTR_LOLEVEL for a logic low, can also do other interrupts, see gpio.h above
+   wifi_set_opmode_current(NULL_MODE);
+   wifi_fpm_set_sleep_type(LIGHT_SLEEP_T); // set sleep type, the above    posters wifi_set_sleep_type() didnt seem to work for me although it did let me compile and upload with no errors 
+   wifi_fpm_open(); // Enables force sleep
+   wifi_fpm_do_sleep(0xFFFFFFF); // Sleep for longest possible time
+   delay(1000);
+   
+   // on Wakeup
+   Serial.print("Wakeup^^");
+   initWifi();
+}
 
 void setup() {
   Serial.begin(115200);
